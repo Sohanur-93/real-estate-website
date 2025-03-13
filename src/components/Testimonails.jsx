635ed6a3949bba506bd6dfaf77432f5edae9172f
Sohanur-93 <mdsohanurrahman3149@gmail.com>
2025-03-13 17:10:23 +0600

    testimonials section start

---
+++ src/components/Testimonails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonails = () => {
+  return (
+    <div className='container mx-auto py-4 px-6 pt-20 md:px-20 lg:px-32 w-full overflow-hidden' id='Testimonails'>
+        <h1 className=' text-2xl sm:text-4xl text-center font-bold mb-2'>Customer <span className='underline underline-offset-4 decoration-1 font-light'>Testimonials</span></h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Real Stories from Those Who Found Home with Us</p>
+    </div>
+  )
+}
+
+export default Testimonails
